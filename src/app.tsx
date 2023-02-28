ba52f8470ea909a761bc39e5605cd5fe09ee5bb3
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-28 13:32:58 -0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center font-mono">
+      <h1 className="font-bold text-xl">Hello</h1>
+    </div>
+  );
+}
