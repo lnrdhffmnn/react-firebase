3f06380ce28a7a32c30a90ba494f0ba0b6bc8572
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-28 18:02:52 -0300

    Add router

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center font-mono">
-      <h1 className="font-bold text-xl">Hello</h1>
+    <div className="w-screen h-screen flex flex-col items-center justify-center font-mono bg-zinc-800 text-white p-8">
+      <Outlet />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./app";
 import "./index.css";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Logout from "./pages/logout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "logout",
+        element: <Logout />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
