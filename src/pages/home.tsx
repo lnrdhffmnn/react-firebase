70637cf0cc7bb578a96e82370b2a4e2b84eff506
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-03-02 11:40:17 -0300

    Update home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,38 @@
+import { useAtomValue } from "jotai";
+import { userAtom } from "../atoms/user";
+import { routes } from "../routes";
+
 export default function Home() {
-  return <p>Home</p>;
+  const user = useAtomValue(userAtom);
+
+  return (
+    <>
+      <img
+        src={user?.photoURL!}
+        alt={user?.displayName!}
+        width={120}
+        height={120}
+        className="rounded-full shadow-md"
+      />
+      <p className="mt-4 font-bold text-xl">{user?.displayName}</p>
+      <p className="text-zinc-300 text-sm">{user?.email}</p>
+      <p className="mt-4">
+        <span className="font-bold">Primeiro login: </span>
+        <span className="text-zinc-300">
+          {new Date(user?.metadata.creationTime!).toLocaleString()}
+        </span>
+      </p>
+      <p>
+        <span className="font-bold">Último login: </span>
+        <span className="text-zinc-300">
+          {new Date(user?.metadata.lastSignInTime!).toLocaleString()}
+        </span>
+      </p>
+      <p className="mt-4">
+        <a href={routes.logout.href} className="text-sky-500 hover:underline">
+          Sair
+        </a>
+      </p>
+    </>
+  );
 }
